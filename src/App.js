e559f5a4aa97ab7c96d1e71c3c8d075ef236f847
akshpnwr <akashpanwar9976@gmail.com>
2021-02-28 07:48:52 +0530

    setting up the development environment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>hi, i'm a react app</h1>
+        <p>Finally i'm learning react</p>
       </div>
     );
   }
