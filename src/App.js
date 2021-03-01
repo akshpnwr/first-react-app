4588d59039ee6c5b82a3d68808e81b3c5a9a81f4
akshpnwr <akashpanwar9976@gmail.com>
2021-03-01 09:58:27 +0530

    stateless and stateful

--- src/App.js
+++ src/App.js
@@ -1,52 +1,96 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 
 import './App.css';
 import Person from './Person/Person';
 
-const app = (props) => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     person: [
       { name: 'Akash', age: 20 },
       { name: 'Aakriti', age: 21 },
-      { name: 'some girl', age: 18 },
+      { name: 'Gnja', age: 22 },
     ],
-  });
-
-  const [otherState, _] = useState('some other value');
-
-  const switchNameHandler = () => {
-    setPersonsState({
-      person: [
-        { name: 'Aksh', age: 20 },
-        { name: 'kriti', age: 21 },
-        { name: 'some girl', age: 18 },
-      ],
-    });
   };
 
-  console.log(personsState, otherState);
+  switchNameHandler(newName) {
+    this.setState({
+      person: [
+        { name: newName, age: 21 },
+        { name: 'kriti', age: 20 },
+        { name: 'Gnja', age: 22 },
+      ],
+    });
+  }
 
-  return (
-    <div className="App">
-      <h1>hi, i'm a react app</h1>
-      <p>Finally i'm learning react</p>
-      <button onClick={switchNameHandler}>Switch name</button>
-      <Person
-        name={personsState.person[0].name}
-        age={personsState.person[0].age}
-      />
-      <Person
-        name={personsState.person[1].name}
-        age={personsState.person[1].age}
-      >
-        And this Person is me.
-      </Person>
-      <Person
-        name={personsState.person[2].name}
-        age={personsState.person[2].age}
-      />
-    </div>
-  );
-};
+  nameChangedHandler(e) {
+    this.setState({
+      person: [
+        { name: 'Akash', age: 21 },
+        { name: e.target.value, age: 20 },
+        { name: 'Gnja', age: 22 },
+      ],
+    });
+  }
 
-export default app;
+  render() {
+    const style = {
+      border: '1px solid black',
+      backgroundColor: 'white',
+      color: 'black',
+      padding: '10px',
+      cursor: 'pointer',
+    };
+    return (
+      <div className="App">
+        <h1>hi, i'm a react app</h1>
+        <p>Finally i'm learning react</p>
+        <button
+          style={style}
+          onClick={this.switchNameHandler.bind(this, 'Aksh')}
+        >
+          Switch name
+        </button>
+        <Person
+          name={this.state.person[0].name}
+          age={this.state.person[0].age}
+        />
+        <Person
+          name={this.state.person[1].name}
+          age={this.state.person[1].age}
+          click={this.switchNameHandler.bind(this, 'Aksh')}
+          changed={this.nameChangedHandler.bind(this)}
+        >
+          And this Person is me.
+        </Person>
+        <Person
+          name={this.state.person[2].name}
+          age={this.state.person[2].age}
+        />
+      </div>
+    );
+  }
+}
+
+// const app = (props) => {
+//   const [personsState, setPersonsState] = useState({
+//     person: [
+//       { name: 'Akash', age: 20 },
+//       { name: 'Aakriti', age: 21 },
+//       { name: 'some girl', age: 18 },
+//     ],
+//   });
+
+//   const [otherState, _] = useState('some other value');
+
+//   const switchNameHandler = () => {
+//     setPersonsState({
+//       person: [
+//         { name: 'Aksh', age: 20 },
+//         { name: 'kriti', age: 21 },
+//         { name: 'some girl', age: 18 },
+//       ],
+//     });
+//   };
+// };
+
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import './Person.css';
+
 const person = (props) => (
-  <div>
+  <div className="Person">
     <p>
       I'm {props.name} and i'm {props.age} years old.
     </p>
-    <p>{props.children}</p>
+    <p onClick={props.click}>{props.children}</p>
+    <input type="text" value={props.name} onChange={props.changed} />
   </div>
 );
 
